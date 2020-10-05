2d0ab62e6f43f1954681f77189a44279aea35fba
きゅん <kyun.anothercastle@gmail.com>
2020-10-05 10:54:39 +0900

    Initial Commit

---
+++ Quizu2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizu2020
+//
+//  Created by きゅん on 2020/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
